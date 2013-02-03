e1c7e989a20dfa58a50fa21a7ee077316dfde1f2
Sniper- <markp.mckinney@gmail.com>
2013-02-02 23:25:01 -0800

    Fix settings

--- src/com/android/settings/PerformanceSettings.java
+++ src/com/android/settings/PerformanceSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.cyanogenmod;
+package com.android.settings;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
